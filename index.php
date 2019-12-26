4eb812c361ec65abe8b374f2c7ab3ce0068b0be2
Mihael Merlin <micka-m78@hotmail.fr>
2019-12-26 21:13:22 +0100

    Adding the last modifications

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Sea Shepherd Exercice</title>
+    <meta name="Sea Shepherd" content="This web site it's just an exercice">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <div id="logo">
+        <a href="https://www.seashepherd.fr/"><img src="pictures\logoSSF.jpg" alt="logo seashepherd"></a>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Qui sommes nous ?</a></li>
+          <li><a href="#">Nos Campagnes</a></li>
+          <li><a href="#">Impliquez-vous</a></li>
+          <li><a href="#">Evenements</a></li>
+          <li><a href="#">Boutique</a></li>
+        </ul>
+      </nav>
+      <div id="help">
+        <div class="support">
+          <p><a href="#">Soutenez-Nous !</a></p>
+        </div>
+      </div>
+    </header>
+    <section id="mainSection">
+      <div class="mainPicture"></div>
+      <div class="quotationContainer">
+        <p>« Il vient une heure où protester ne suffit plus, après la philosophie, il faut l’action. »</p>
+        <p>Victor Hugo</p>
+      </div>
+      <div class="introductionContainer">
+        <div class="introduction">
+          <p class="firstLastP">Fondée en 1977 par le capitaine Paul Watson, SEA SHEPHERD est l'ONG de défense des océans la plus combative au monde.</p>
+          <p class="secondP">Sea Shepherd travaille sur trois axes majeurs :</p>
+          <p><span class="bold">Dépasser la seule protestation</span class="bold"> et intervenir de manière active et non violente dans les cas d'<span class="bold">atteintes illégales</span> à la vie marine et aux écosystèmes marins</p>
+          <p><span class="bold">Exposer les abus</span> et les pratiques non durables ou non éthiques d'atteinte à la vie marine et à l'intégrité des écosystèmes marins <span class="bold">en alertant les médias</span> et l'opinion publique.</p>
+          <p><span class="bold">Sensibiliser l'opinion publique</span> au lien essentiel qui nous relie à l'océan à travers diverses interventions en festivals, écoles, organisation de conférences, expositions, publications, films, etc...</p>
+          <p class="firstLastP">Sea Shepherd est <span class="bold">100% indépendante</span> et ne bénéficie d'aucune subvention d'Etat afin de garantir sa <span class="bold">liberté de parole et d'action</span>. Les dons faits à l'association permettent en revanche de bénéficier d'une déduction fiscale de 66%</p>
+          <p class="button"><a class="bold" href="/index.php/Soutenez-Nous/">Faire un don</a></p>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
